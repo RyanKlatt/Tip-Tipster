21f7c5c9e7f59aa1a5f5278731e143661ba671c6
Ryan Klatt <rpklatt@msn.com>
2017-07-16 09:48:49 -0600

    Adding Functions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,15 +7,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+	total: number;
+	tip : number;
+	tipTotal : number;
+
   constructor(public navCtrl: NavController) {
 
   }
-  notGreat(){}
-  average(){}
-  good(){}
-  veryGood(){}
-  excellent(){}
-  roundDown(){}
-  roundUp(){}
- 
+  notGreat() {
+        this.tip = this.total * 0.075;
+        this.tipTotal = this.total + this.tip;
+    };
+    
+    average() {
+        this.tip = this.total * 0.15;
+        this.tipTotal = this.total + this.tip;
+    };
+    
+    good() {
+        this.tip = this.total * 0.20;
+        this.tipTotal = this.total + this.tip;
+    };
+    
+    veryGood() {
+        this.tip = this.total * 0.25;
+        this.tipTotal = this.total + this.tip;
+    };
+    
+    excellent() {
+        this.tip = this.total * 0.35;
+        this.tipTotal = this.total + this.tip;
+    };
+    
+    roundDown() {
+        this.tip = Math.floor(this.tip);
+        this.tipTotal = this.total + this.tip;
+    };
+    
+    roundUp() {
+        this.tip = Math.ceil(this.tip);
+        this.tipTotal = this.total + this.tip;
+    };
+
 }
