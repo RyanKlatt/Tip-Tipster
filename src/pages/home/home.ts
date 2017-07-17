df9c242ccc660d3d5097bea1f54cdbbfbb4383b3
Ryan Klatt <rpklatt@msn.com>
2017-07-16 19:50:43 -0600

    Completed Functions + Styling

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,39 +14,40 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
   notGreat() {
-        this.tip = this.total * 0.075;
-        this.tipTotal = this.total + this.tip;
+        this.tip = this.total * 0.05;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
     
     average() {
         this.tip = this.total * 0.15;
-        this.tipTotal = this.total + this.tip;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
     
     good() {
         this.tip = this.total * 0.20;
-        this.tipTotal = this.total + this.tip;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
     
     veryGood() {
         this.tip = this.total * 0.25;
-        this.tipTotal = this.total + this.tip;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
     
     excellent() {
         this.tip = this.total * 0.35;
-        this.tipTotal = this.total + this.tip;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
     
     roundDown() {
         this.tip = Math.floor(this.tip);
-        this.tipTotal = this.total + this.tip;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
     
     roundUp() {
         this.tip = Math.ceil(this.tip);
-        this.tipTotal = this.total + this.tip;
+        this.tipTotal = Number(this.total) + Number(this.tip);
     };
 
 }
